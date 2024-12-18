3dfec521d381d3e75bfa65e1438ecfeaf370dda3
Orashi6 <bekb0323@gmail.com>
2024-12-18 18:08:37 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,50 @@
 function order() {
-    let numb = +prompt('nechta buyurtma bermoqchisiz?')
+     let numb = +prompt('nechta buyurtma bermoqchisiz?')
 
-    let qabul = confirm(`${numb} ta Qiyqiriq set ${numb*45000} som   \n\n Buyurtmani qabul qilasizmi?`)
+    // let qabul = confirm(`${numb} ta Qiyqiriq set ${numb*45000} som   \n\n Buyurtmani qabul qilasizmi?`)
 
 
-    if (qabul == true) {
-        alert('Zakaz qabul qilindi')
-    } else {
-        alert('Zakaz otmen')
+    // if (qabul == true) {
+    //     alert('Zakaz qabul qilindi')
+    // } else {
+    //     alert('Zakaz otmen')
+    // }
+
+let nechta = numb*45000
+
+    switch (numb) {
+        case 1:
+            confirm(`${numb} ta Qiyqiriq set ${nechta} som zakaz qilasizmi?`)
+            break;
+            case 2:
+            confirm(`${numb} ta Qiyqiriq set ${nechta} som zakaz qilasizmi?`)
+            break;
+            case 3:
+            confirm(`${numb} ta Qiyqiriq set ${nechta} som zakaz qilasizmi?`)
+            break;
+            case 4:
+            confirm(`${numb} ta Qiyqiriq set ${nechta} som zakaz qilasizmi?`)
+            break;
+            case 5:
+            confirm(`${numb} ta Qiyqiriq set ${nechta} som zakaz qilasizmi?`)
+            break;
+            case 6:
+            confirm(`${numb} ta Qiyqiriq set ${nechta} som zakaz qilasizmi?`)
+            break;
+            case 7:
+            confirm(`${numb} ta Qiyqiriq set ${nechta} som zakaz qilasizmi?`)
+            break;
+            case 8:
+            confirm(`${numb} ta Qiyqiriq set ${nechta} som zakaz qilasizmi?`)
+            break;
+            case 9:
+            confirm(`${numb} ta Qiyqiriq set ${nechta} som zakaz qilasizmi?`)
+            break;
+            case 10:
+            confirm(`${numb} ta Qiyqiriq set ${nechta} som zakaz qilasizmi?`)
+            break;
+    
+        default:
+            break;
     }
 }
